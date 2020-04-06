2de655ad4b23300bbd0df527f7026ce7dd009014
tanisk jha <jhatanishqq@outlook.com>
2020-04-06 18:16:50 +0530

    refactored nav

--- src/Components/BB.js
+++ src/Components/BB.js
@@ -14,36 +14,49 @@ function BB() {
     })
     return (
         <div >
-            <table class="table table-striped table-bordered table-hover" id="country-detail">
-                <thead class="thead bg-warning">
-                    <tr>
+            <div>
+                <div class="col-xl-12 col-lg-12">
+                    <div class="card shadow mb-4" id="table-card">
 
-                        <th scope="col">Countries</th>
-                        <th scope="col">Confirmed Cases</th>
-                        <th scope="col">Total Death</th>
-                        <th scope="col">Total Recovered</th>
-                    </tr>
-                </thead>
-                <tbody className='border'>
-                    <tr >
+                        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between"></div>
 
-                        <td>
-                            {coronas.map(corona => <strong> <p key={corona.CountryCode}>{corona.Country} </p> </strong>)}
-                        </td>
 
-                        <td> {coronas.map(corona => <p key={corona.CountryCode}>{corona.TotalConfirmed}</p>)}
-                        </td>
 
-                        <td> {coronas.map(corona => <p key={corona.CountryCode}>{corona.TotalDeaths}</p>)}
-                        </td>
+                        <table class="table table-striped table-bordered table-hover" id="country-detail">
+                            <thead class="thead bg-warning">
+                                <tr>
+                                    <th scope="col">Countries</th>
+                                    <th scope="col">Confirmed Cases</th>
+                                    <th scope="col">Total Death</th>
+                                    <th scope="col">Total Recovered</th>
+                                </tr>
+                            </thead>
+                            <tr>
 
-                        <td>   {coronas.map(corona => <p key={corona.CountryCode}>{corona.TotalRecovered}</p>)}
-                        </td>
-                    </tr>
+                                <td>
+                                    {coronas.map(corona => <strong> <p key={corona.CountryCode}>{corona.Country} </p> </strong>)}
+                                </td>
 
-                </tbody>
-            </table>
 
+
+                                <td> {coronas.map(corona => <p key={corona.CountryCode}>{corona.TotalConfirmed}</p>)}
+                                </td>
+
+
+                                <td> {coronas.map(corona => <p key={corona.CountryCode}>{corona.TotalDeaths}</p>)}
+                                </td>
+
+                                <td>   {coronas.map(corona => <p key={corona.CountryCode}>{corona.TotalRecovered}</p>)}
+                                </td>
+
+
+                            </tr>
+
+                        </table>
+
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
